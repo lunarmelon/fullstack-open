1ca289f00e5d2dae7fe9f8dc212eb55db6ab6a8b
lunarmelon <lunarmelon@tuta.io>
2026-08-17 15:24:01 -0600

    exercise 1.2 correction

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -15,18 +15,16 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
-  const part1 = 'Fundamentals of React'
-  const part2 = 'Using props to pass data'
-  const part3 = 'State of a component'
+
   const exercises1 = 10
   const exercises2 = 7
   const exercises3 = 14
   
   return (
     <div>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
   )
 }
@@ -45,10 +43,14 @@ const App = () => {
   const exercises2 = 7
   const exercises3 = 14
 
+  const part1 = 'Fundamentals of React'
+  const part2 = 'Using props to pass data'
+  const part3 = 'State of a component'
+
   return (
     <div>
       <Header course={course} />
-      <Content />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
