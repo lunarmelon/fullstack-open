e580320a14b3a1d0b61d1dbac7eb5411a9cde958
lunarmelon <lunarmelon@tuta.io>
2026-08-19 19:13:43 -0600

    exercise 1.7 - unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,24 +11,30 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad,setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
 
   const handleGood = () => {
     const updatedGood = good + 1;
     setGood(updatedGood);
-    console.log("good: " + updatedGood);
+    setAverage(average + 1);
+    setTotal(updatedGood + neutral + bad);
   }
 
   const handleNeutral = () => {
     const updatedNeutral = neutral + 1;
     setNeutral(updatedNeutral);
-    console.log("neutral: " + updatedNeutral);
+    setTotal(good + updatedNeutral + bad);
+    setAverage(average);
   } 
   
   const handleBad = () => {
     const updatedBad = bad + 1;
     setBad(updatedBad);
-    console.log("bad: " + updatedBad);
+    setTotal(good + neutral + updatedBad);
+    setAverage(average - 1);
   }
+
   return (
     <div>
       <h1>Give feedback</h1>
@@ -36,9 +42,12 @@ const App = () => {
       <Button onClick={handleNeutral} text={"neutral"}/>
       <Button onClick={handleBad} text={"bad"}/>
       <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>Average: {average/total}</p>
+      <p>Positive: {good/total * 100}%</p>
     </div>
   )
 }
