2a395d80cb2ef0e47c64bbd8b0d37f5e5a71ef1c
lunarmelon <lunarmelon@tuta.io>
2026-08-20 20:16:09 -0600

    exercise 2.10 - phonebook step 5

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import PersonForm from './components/PersonForm'
+import Filter from './components/Filter'
+import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -49,23 +52,17 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      filter shown with <input value={search} onChange={handleSearchChange} />
+      <Filter search={search} handleSearchChange={handleSearchChange} />
       <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      /> 
       <h2>Numbers</h2>
-      {personsToShow.map(person => 
-        <p key={person.id}>{person.name} {person.number}</p>
-      )}
+      <Persons personsToShow={personsToShow} />
     </div>
   )
 }
---
+++ part2/phonebook/src/components/Filter.jsx
@@ -0,0 +1,9 @@
+const Filter = (props) => {
+  return (
+    <div>
+      filter shown with <input value={props.search} onChange={props.handleSearchChange} />
+    </div>
+  )
+}
+
+export default Filter
