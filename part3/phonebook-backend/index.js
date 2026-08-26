8ca080a7ba399658f83deb2430180cb54b586587
lunarmelon <lunarmelon@tuta.io>
2026-08-25 21:48:52 -0600

    exercise 3.2 - phonebook backend step 2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -28,6 +28,15 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const date = new Date()
+  response.send(`
+    Phonebook has info for ${persons.length} people
+    <p>${date}</p>
+    `
+  )
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
