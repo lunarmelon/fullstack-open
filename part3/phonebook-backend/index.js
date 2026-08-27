6948f5948aaba7368bf128343e5e95255f773c2c
lunarmelon <lunarmelon@tuta.io>
2026-08-26 18:46:27 -0600

    exercise 3.4 - phonebook backend step 4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,54 +1,61 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const persons = [
-  { 
-    "id": "1",
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": "2",
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": "3",
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": "4",
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+let persons = [
+	{
+		id: "1",
+		name: "Arto Hellas",
+		number: "040-123456",
+	},
+	{
+		id: "2",
+		name: "Ada Lovelace",
+		number: "39-44-5323523",
+	},
+	{
+		id: "3",
+		name: "Dan Abramov",
+		number: "12-43-234345",
+	},
+	{
+		id: "4",
+		name: "Mary Poppendieck",
+		number: "39-23-6423122",
+	},
+];
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
+app.get("/api/persons", (request, response) => {
+	response.json(persons);
+});
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-  
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
-})
+app.get("/api/persons/:id", (request, response) => {
+	const id = request.params.id;
+	const person = persons.find((person) => person.id === id);
 
-app.get('/info', (request, response) => {
-  const date = new Date()
-  response.send(`
+	if (person) {
+		response.json(person);
+	} else {
+		response.status(404).end();
+	}
+});
+
+app.get("/info", (request, response) => {
+	const date = new Date();
+	response.send(`
     Phonebook has info for ${persons.length} people
     <p>${date}</p>
-    `
-  )
-})
+    `);
+});
 
-const PORT = 3001
+app.delete("/api/persons/:id", (request, response) => {
+	const id = request.params.id;
+	persons = persons.filter((person) => person.id !== id);
+
+	response.status(204).end();
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+	console.log(`Server running on port ${PORT}`);
+});
+
