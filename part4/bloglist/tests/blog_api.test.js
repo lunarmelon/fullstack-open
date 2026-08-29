2b813dc00effccdbbde11ae1c11faa18d7f62f6a
lunarmelon <lunarmelon@tuta.io>
2026-08-28 19:07:36 -0600

    exercise 4.11 - blog list tests step 4

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -66,6 +66,22 @@ test("a valid blog can be added", async () => {
 	assert(contents.includes("Miles Edgeworth"));
 });
 
+test("missing likes property defaults to 0", async () => {
+	const newBlog = {
+		title: "Go To Statement Considered Harmful",
+		author: "Miles Edgeworth",
+		url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+	};
+
+	const addedBlog = await api
+		.post("/api/blogs")
+		.send(newBlog)
+		.expect(201)
+		.expect("Content-Type", /application\/json/);
+
+	assert.strictEqual(addedBlog.body.likes, 0);
+});
+
 after(async () => {
 	await mongoose.connection.close();
 });
