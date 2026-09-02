e993a7c649cb107ef13bfe62b02cb45ca56a8240
lunarmelon <lunarmelon@tuta.io>
2026-09-01 19:11:52 -0600

    exercise 5.3 - blog list frontend step 3

--- part4/bloglist-frontend/src/App.jsx
+++ part4/bloglist-frontend/src/App.jsx
@@ -5,6 +5,9 @@ import loginService from "./services/login";
 
 const App = () => {
 	const [blogs, setBlogs] = useState([]);
+	const [title, setTitle] = useState("");
+	const [author, setAuthor] = useState("");
+	const [url, setUrl] = useState("");
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 	const [user, setUser] = useState(null);
@@ -22,18 +25,40 @@ const App = () => {
 		}
 	}, []);
 
+	const addBlog = (event) => {
+		event.preventDefault();
+		const blogObject = {
+			title: title,
+			author: author,
+			url: url,
+		};
+
+		blogService.create(blogObject).then((returnedBlog) => {
+			setBlogs(blogs.concat(returnedBlog));
+			setTitle("");
+			setAuthor("");
+			setUrl("");
+		});
+	};
+
 	const handleLogin = async (event) => {
 		event.preventDefault();
 
 		try {
 			const user = await loginService.login({ username, password });
 			window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
+			blogService.setToken(user.token);
 			setUser(user);
 			setUsername("");
 			setPassword("");
 		} catch {
 			console.log("wrong credentials");
 		}
+	};
+
+	const handleLogout = async (event) => {
+		event.preventDefault();
+		window.localStorage.removeItem("loggedBlogappUser");
 	};
 
 	const loginForm = () => (
@@ -65,6 +90,40 @@ const App = () => {
 		</div>
 	);
 
+	const blogForm = () => (
+		<form onSubmit={addBlog}>
+			<h2>create new</h2>
+			<label>
+				title
+				<input
+					type="text"
+					value={title}
+					onChange={({ target }) => setTitle(target.value)}
+				/>
+			</label>
+			<br />
+			<label>
+				author
+				<input
+					type="text"
+					value={author}
+					onChange={({ target }) => setAuthor(target.value)}
+				/>
+			</label>
+			<br />
+			<label>
+				url
+				<input
+					type="text"
+					value={url}
+					onChange={({ target }) => setUrl(target.value)}
+				/>
+			</label>
+			<br />
+			<button type="submit">create</button>
+		</form>
+	);
+
 	const blogList = () => (
 		<div>
 			<h2>blogs</h2>
@@ -77,6 +136,13 @@ const App = () => {
 	return (
 		<div>
 			{!user && loginForm()}
+			{user && (
+				<div>
+					<p>{user.name} logged in</p>
+					<button onClick={handleLogout}>logout</button>
+					{blogForm()}
+				</div>
+			)}
 			{user && blogList()}
 		</div>
 	);
