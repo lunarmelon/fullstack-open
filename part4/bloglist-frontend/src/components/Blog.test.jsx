0078b172f9c9ffb170725280b0d2e5330fc719c8
lunarmelon <lunarmelon@tuta.io>
2026-09-03 16:04:58 -0600

    exercise 5.15 - blog list tests step 3

--- part4/bloglist-frontend/src/components/Blog.test.jsx
+++ part4/bloglist-frontend/src/components/Blog.test.jsx
@@ -9,11 +9,9 @@ describe("<Blog />", () => {
 		url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
 		likes: 23,
 	};
-	beforeEach(() => {
-		render(<Blog blog={blog} />);
-	});
 
 	test("only renders the blog's title and author by default", () => {
+		render(<Blog blog={blog} />);
 		expect(
 			screen.getByText(/Go To Statement Considered Harmful/),
 		).toBeVisible();
@@ -24,11 +22,28 @@ describe("<Blog />", () => {
 	});
 
 	test("after clicking the button, url and likes are displayed", async () => {
+		render(<Blog blog={blog} />);
 		const user = userEvent.setup();
 		const button = screen.getByText("view");
 		await user.click(button);
 
 		expect(screen.getByText(/homepages.cwi.nl/)).toBeVisible();
 		expect(screen.getByText(/likes 23/)).toBeVisible();
+	});
+
+	test("likes event handler is called twice if like button is clicked twice", async () => {
+		const like = vi.fn();
+		const user = userEvent.setup();
+
+		render(<Blog blog={blog} addLike={like} />);
+
+		const detailsButton = screen.getByText("view");
+		await user.click(detailsButton);
+
+		const likeButton = screen.getByText("like");
+		await user.click(likeButton);
+		await user.click(likeButton);
+
+		expect(like.mock.calls).toHaveLength(2);
 	});
 });
